27f3feb27c7809fd4d3525adea5391f3894db43a
Shane Upchurch <shaneupchurch@gmail.com>
2020-12-09 19:26:18 -0600

    both models, all routes, all tests passing

--- tests/app.test.js
+++ tests/app.test.js
@@ -36,6 +36,126 @@ describe('app endpoint', () => {
         expect(response.body).toEqual(newVeggie);
     });
 
+    it('GETs all veggies', async () => {
+        const allVeggies = await Promise.all([
+            {
+                name: 'cucumber'
+            },
+            {
+                name: 'squash'
+            },
+            {
+                name: 'corn'
+            }
+        ].map(veggie => Veggie.insert(veggie)));
+
+        const response = await request(app)
+            .get('/veggies');
+
+        expect(response.body).toEqual(expect.arrayContaining(allVeggies));
+        expect(response.body).toHaveLength(allVeggies.length);
+    });
+
+    it('updates a veggie by id via PUT', async () => {
+        const newVeggie = await Veggie.insert({ name: 'corn' });
+
+        const response = await request(app)
+            .put(`/veggies/${newVeggie.id}`)
+            .send({ name: 'cauliflower' });
+
+        expect(response.body).toEqual({
+            id: newVeggie.id,
+            name: 'cauliflower'
+        });
+    });
+
+    it('Deletes a veggie by id via DELETE', async () => {
+        const newVeggie = await Veggie.insert({ name: 'potato' });
+
+        const response = await request(app)
+            .delete(`/veggies/${newVeggie.id}`);
+
+        expect(response.body).toEqual(newVeggie);
+
+    });
+
+
+
+    it('creates a new Farmshare via POST', async () => {
+        const newVeggie = await Veggie.insert({ name: 'tomato' });
+
+        const res = await request(app)
+            .post('/farmshares')
+            .send({ name: 'Smith', veggieId: newVeggie.id });
+
+        expect(res.body).toEqual({
+            id: '1',
+            name: 'Smith',
+            veggieId: newVeggie.id
+        });
+    });
+
+    it('GETs a single farmshare by Id', async () => {
+        const newVeggie = await Veggie.insert({ name: 'cucumber' });
+
+        const newFarmshare = await Promise.all([
+            { name: 'Smith', veggieId: newVeggie.id },
+            { name: 'Elvis', veggieId: newVeggie.id },
+            { name: 'Big Boi', veggieId: newVeggie.id }
+        ].map(farmshare => Farmshare.insert(farmshare)));
+
+        const res = await request(app)
+            .get('/farmshares/1');
+
+        expect(res.body).toEqual(newFarmshare[0]);
+    });
+
+    it('GETs all farmshares', async () => {
+        const newVeggie = await Veggie.insert({ name: 'cucumber' });
+
+        const newFarmshare = await Promise.all([
+            { name: 'Smith', veggieId: newVeggie.id },
+            { name: 'Elvis', veggieId: newVeggie.id },
+            { name: 'Big Boi', veggieId: newVeggie.id }
+        ].map(farmshare => Farmshare.insert(farmshare)));
+
+        const res = await request(app)
+            .get('/farmshares');
+
+        expect(res.body).toEqual(newFarmshare);
+    });
+
+    it('updates an existing farmshare by id via PUT', async () => {
+        const newVeggie = await Veggie.insert({ name: 'cucumber' });
+
+        const newFarmshare = await Promise.all([
+            { name: 'Smith', veggieId: newVeggie.id },
+            { name: 'Elvis', veggieId: newVeggie.id },
+            { name: 'Big Boi', veggieId: newVeggie.id }
+        ].map(farmshare => Farmshare.insert(farmshare)));
+
+        const res = await request(app)
+            .put(`/farmshares/${newFarmshare[0].id}`)
+            .send({ name: 'Brown', veggieId: newVeggie.id });
+
+        expect(res.body).toEqual({ id: '1', name: 'Brown', veggieId: newVeggie.id });
+    });
+
+    it('deletes an existing farmshare by id', async () => {
+        const newVeggie = await Veggie.insert({ name: 'cucumber' });
+
+        const newFarmshare = await Promise.all([
+            { name: 'Smith', veggieId: newVeggie.id },
+            { name: 'Elvis', veggieId: newVeggie.id },
+            { name: 'Big Boi', veggieId: newVeggie.id }
+        ].map(farmshare => Farmshare.insert(farmshare)));
+
+        const res = await request(app)
+            .delete(`/farmshares/${newFarmshare[2].id}`);
+
+        expect(res.body).toEqual(newFarmshare[2]);
+
+    });
 
 });
 
